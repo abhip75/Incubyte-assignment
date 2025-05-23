94ed2c28339c8093584eaf46ae620c446a957956
abhip75 <abhishekpwr944@gmail.com>
2025-05-23 17:07:01 +0530

    allow add method to handle any amount of numbers

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,10 @@
 function add(numbers){
     if(numbers==='') return 0;
+
+    const nums = numbers.split(',').map(n => parseInt(n,10));
+    const sum = nums.reduce((acc,num) => acc + num, 0);
+
+    return sum;
 }
 
 module.exports = { add}
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -10,4 +10,10 @@ test('returns 1 for "1"', () => {
 
 test('returns 6 for "1,5"',() => {
     expect(add("1,5")).toBe(6);
+});
+
+test('returns sum of multiple numbers', () => {
+    expect(add("1,2,3")).toBe(6);
+    expect(add("0,2,0")).toBe(2);
+    expect(add("10,20,30")).toBe(60);
 })
